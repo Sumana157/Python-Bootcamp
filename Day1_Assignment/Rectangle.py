568bab9c1fa3078d71d908c01aa7911eac2fd68b
Sumana157 <sumana.jb15@gmail.com>
2025-05-21 21:58:13 +0530

    Add files via upload

---
+++ Day1_Assignment/Rectangle.py
@@ -0,0 +1,6 @@
+Rect_length = float(input("Enter the length of the rectangle: "))
+Rect_width = float(input("Enter the width of the rectangle: "))
+
+Rect_area = Rect_length * Rect_width
+
+print("The area of the rectangle is:", Rect_area)
